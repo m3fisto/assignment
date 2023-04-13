d4ba1c7207aea197cb7797746da38c8f0903e799
Paris Malakounidis <paris.malakounidis@oddschecker.com>
2023-04-13 07:13:25 +0300

    slim down image

--- Tests/conf_test.py
+++ Tests/conf_test.py
@@ -7,9 +7,7 @@ url = data.fullUrl
 
 def is_responsive(url):
     try:
-        print(url)
         response = requests.get(url)
-        print (response.status_code)
         if response.status_code == 200:
             return True
     except ConnectionError:
@@ -17,13 +15,12 @@ def is_responsive(url):
 
 
 @pytest.fixture(scope="session")
-def http_service(docker_services):
+def api_sw_service(docker_services):
     docker_services.wait_until_responsive(
         timeout=30.0, pause=0.1, check=lambda: is_responsive(url)
     )
     return url
 
-def test_status_container(http_service,docker_services):
+def test_status_container(api_sw_service,docker_services):
     response = requests.get(url)
-    print (response.status_code)
-    assert response.status_code == 200
+    assert response.json() == data.people
--- dockerfile
+++ dockerfile
@@ -5,5 +5,5 @@ RUN apk add --no-cache python3-dev
 RUN pip install --upgrade pip
 WORKDIR /app
 COPY . /app
-RUN pip --no-cache-dir install -r requirements.txt
+RUN pip install flask
 CMD ["python3", "src/app.py"]
